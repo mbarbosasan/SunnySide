6248523e113bdc6a466bf295e37444f188fd698e
Tokenph <moisesbarbosa23@gmail.com>
2021-08-28 20:50:24 -0300

    Corrigido alguns problemas de acessiblidade e HTML

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const btnMobile = document.querySelector('.btn-mobile');
 btnMobile.addEventListener('click', toggleMenu)
 
 function toggleMenu() {
-  const nav = document.querySelector('#nav');
+  const nav = document.querySelector('#navbar');
   nav.classList.toggle('active');
 }
